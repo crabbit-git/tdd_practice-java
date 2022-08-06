864fa0eaabe341a0d4ae8279ae509f3afa22d3ed
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-08-06 23:29:19 +0100

    More efficient Log Levels

--- log-levels/src/main/java/LogLevels.java
+++ log-levels/src/main/java/LogLevels.java
@@ -7,7 +7,7 @@ public class LogLevels
 
     public static String logLevel(String logLine)
     {
-        return logLine.split("]")[0].substring(1).toLowerCase();
+        return logLine.split("\\[|\\]")[1].toLowerCase();
     }
 
     public static String reformat(String logLine)
